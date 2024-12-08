e88e16855459216cf0cef3362d946a651c66e76f
Damian0099 <damianvinodh@gmail.com>
2024-12-08 14:04:22 +0000

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,11 @@
+print("welcome to python quiz")
+print("how any continents are there?")
+print("1 five")
+print("2 six")
+print("3 seven")
+print("4 eight")
+ans=input("enter your answer :")
+if ans=="3":
+    print("hey...correct answer")
+else:
+    print("oops...wrong answer")
